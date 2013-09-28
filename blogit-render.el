423e8f328a7fdf395077ca49780f0eefda9d7cba
coldnew <coldnew.tw@gmail.com>
2013-09-28 08:19:10 +0800

    add blogit timestring fix

--- blogit-render.el
+++ blogit-render.el
@@ -80,6 +80,98 @@
        ("CONTENT" (org-export-as 'html nil nil t nil))
        )))
 
+(defun blogit-timestamp-string-fix (date-string)
+  "Returns yyyy/mm/dd format of date-string
+For examples:
+   [Nov. 28, 1994]     => [1994/11/28]
+   [November 28, 1994] => [1994/11/28]
+   [11/28/1994]        => [1994/11/28]
+Any \"day of week\", or \"time\" info, or any other parts of the string, are
+discarded.
+
+This function is used to create directory for new blog post.
+"
+  (let ((date-str date-string)
+        date-list year month date yyyy mm dd)
+    (setq date-str (replace-regexp-in-string "^ *\\(.+\\) *$" "\\1" date-str))
+    (cond
+     ;; USA convention of mm/dd/yyyy
+     ((string-match
+       "^\\([0-9][0-9]\\)/\\([0-9][0-9]\\)/\\([0-9][0-9][0-9][0-9]\\)$"
+       date-str)
+      (concat (match-string 3 date-str) "/" (match-string 1 date-str) "/"
+              (match-string 2 date-str)))
+     ((string-match
+       "^\\([0-9]\\)/\\([0-9][0-9]\\)/\\([0-9][0-9][0-9][0-9]\\)$"
+       date-str)
+      (concat (match-string 3 date-str) "/" (match-string 1 date-str) "/"
+              (match-string 2 date-str)))
+     ;; some ISO 8601. yyyy-mm-dd
+     ((string-match
+       "^\\([0-9][0-9][0-9][0-9]\\)-\\([0-9][0-9]\\)-\\([0-9][0-9]\\)$\
+T[0-9][0-9]:[0-9][0-9]" date-str)
+      (concat (match-string 1 date-str) "/" (match-string 2 date-str) "/"
+              (match-string 3 date-str)))
+     ((string-match
+       "^\\([0-9][0-9][0-9][0-9]\\)-\\([0-9][0-9]\\)-\\([0-9][0-9]\\)$"
+       date-str)
+      (concat (match-string 1 date-str) "/" (match-string 2 date-str) "/"
+              (match-string 3 date-str)))
+     ((string-match "^\\([0-9][0-9][0-9][0-9]\\)-\\([0-9][0-9]\\)$" date-str)
+      (concat (match-string 1 date-str) "-" (match-string 2 date-str)))
+     ((string-match "^\\([0-9][0-9][0-9][0-9]\\)$" date-str)
+      (match-string 1 date-str))
+     (t (progn
+          (setq date-str
+                (replace-regexp-in-string "January " "Jan. " date-str))
+          (setq date-str
+                (replace-regexp-in-string "February " "Feb. " date-str))
+          (setq date-str
+                (replace-regexp-in-string "March " "Mar. " date-str))
+          (setq date-str
+                (replace-regexp-in-string "April " "Apr. " date-str))
+          (setq date-str
+                (replace-regexp-in-string "May " "May. " date-str))
+          (setq date-str
+                (replace-regexp-in-string "June " "Jun. " date-str))
+          (setq date-str
+                (replace-regexp-in-string "July " "Jul. " date-str))
+          (setq date-str
+                (replace-regexp-in-string "August " "Aug. " date-str))
+          (setq date-str
+                (replace-regexp-in-string "September " "Sep. " date-str))
+          (setq date-str
+                (replace-regexp-in-string "October " "Oct. " date-str))
+          (setq date-str
+                (replace-regexp-in-string "November " "Nov. " date-str))
+          (setq date-str
+                (replace-regexp-in-string "December " "Dec. " date-str))
+          (setq date-str
+                (replace-regexp-in-string " 1st," " 1" date-str))
+          (setq date-str
+                (replace-regexp-in-string " 2nd," " 2" date-str))
+          (setq date-str
+                (replace-regexp-in-string " 3rd," " 3" date-str))
+          (setq date-str
+                (replace-regexp-in-string "\\([0-9]\\)th," "\\1" date-str))
+          (setq date-str
+                (replace-regexp-in-string " 1st " " 1 " date-str))
+          (setq date-str
+                (replace-regexp-in-string " 2nd " " 2 " date-str))
+          (setq date-str
+                (replace-regexp-in-string " 3rd " " 3 " date-str))
+          (setq date-str
+                (replace-regexp-in-string "\\([0-9]\\)th " "\\1 " date-str))
+          (setq date-list (parse-time-string date-str))
+          (setq year (nth 5 date-list))
+          (setq month (nth 4 date-list))
+          (setq date (nth 3 date-list))
+          (setq yyyy (number-to-string year))
+          (setq mm (if month (format "%02d" month) ""))
+          (setq dd (if date (format "%02d" date) ""))
+          (concat yyyy "/" mm "/" dd))))))
+
+
 (defun blogit-generate-url ()
   ()
   (concat
