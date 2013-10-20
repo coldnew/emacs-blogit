8934b39547c4babc589f95d08e7ea658b98ada99
coldnew <coldnew.tw@gmail.com>
2013-10-20 10:58:30 +0800

    modify font path to fonts

--- sample/style/get-font.sh
+++ sample/style/get-font.sh
@@ -5,7 +5,7 @@ if test -z "$1"; then
     exit 1
 fi
 
-FONT_DIR=font
+FONT_DIR=fonts
 
 FONT_URL="$1"
 LESS_FILE=$(echo "$FONT_URL" | sed -n 's/.*family=\([^:]\+\).*/\1/p' | sed 's/+/-/g')
@@ -20,4 +20,4 @@ echo "${CSS}" | sed -n "s#.*local('\([^']\+\)'), url('\([^']\+\)').*#curl -o ${F
 
 echo "${CSS}" | sed  "s#\(.*\)local('\([^']\+\)'), url('\([^']\+\)')\(.*\)#\1local('\2'), url('../font/\2.woff')\4#"  > "${FONT_DIR}/font-${LESS_FILE}.less"
 
-echo "You should add '@import \"../font/font-${LESS_FILE}.less\";' to style/source/font.less"
+echo "You should add '@import \"../font/fonts-${LESS_FILE}.less\";' to style/source/fonts.less"
