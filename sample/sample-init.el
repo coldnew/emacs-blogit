47d4b294608e9ff45984f38db26d4ecc623053c7
coldnew <coldnew.tw@gmail.com>
2013-10-20 12:23:18 +0800

    post: Add Using modal source code

---
+++ sample/sample-init.el
@@ -0,0 +1,16 @@
+
+(require 'blogit)
+
+(setq blogit-project-alist
+      '(("blogit"
+         :base-directory "The source directory"
+         :publishing-directory "Directory you want to place output file"
+         :blog-url "Your blog url"
+         :blog-title "Blog title"
+
+         :default-language "The most use language in your post"
+
+         :google-analytics "Google analytics id"
+         :disqus    "Disqus short name"
+         :lloogg "lloogg id"
+         )))
