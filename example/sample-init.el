7f4cdf6be9e181eda4403403062a7d629953f53f
coldnew <coldnew.tw@gmail.com>
2013-10-26 01:03:08 +0800

    make headerfile use css

--- example/sample-init.el
+++ example/sample-init.el
@@ -1,4 +1,3 @@
-
 (require 'blogit)
 
 (setq blogit-project-alist
@@ -12,5 +11,25 @@
 
          :google-analytics "Google analytics id"
          :disqus    "Disqus short name"
-         :lloogg "lloogg id"
+         :lloogg    "lloogg id"
+
+         :after-publish-hook my/blogit-after-publish-hook
          )))
+
+(defvar my/blogit-after-publish-hook nil)
+
+(defun my/blogit-compile-less-source ()
+  (let ((output-dir (blogit-project-info :publishing-directory))
+        (output-style-dir
+         (concat output-dir "/" (blogit-project-info :style-directory-name)))
+        (input-file
+         (concat output-style-dir "/source/blogit.less"))
+        (output-file
+         (concat output-style-dir "/source/blogit.css"))
+        (lessc
+         (concat output-style-dir "/less.js/bin/lessc")))
+
+    ;; compile blog.less to blog.css and add it to css folder
+    (shell-command (concat lessc " -x " input-file " > " output-file))))
+
+(add-hook 'my/blogit-after-publish-hook 'my/blogit-compile-less-source)
