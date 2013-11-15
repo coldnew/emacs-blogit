683bd81920229a40f97d117a61c5d2ed82706434
coldnew <coldnew.tw@gmail.com>
2013-11-15 21:26:00 +0800

    color root prompt

--- example/style/js/blogit-extra.js
+++ example/style/js/blogit-extra.js
@@ -1,14 +1,18 @@
 
 // Color Bash prompt in example block
 function colorShellPrompt () {
-    var reSearch = /(\w*@\w*)\s*([:](.+)\/([^/]+)[#$])/;
-    var reReplace = '<font color=\"lightgreen\">$1</font><font color=\"lightblue\">$2</font>';
+    var userHighlight  = '<font color=\"lightgreen\">$1</font><font color=\"lightblue\">$2</font>';
+    var rootHighlight  = '<font color=\"crimson\">$1</font><font color=\"lightblue\">$2</font>';
     var block = document.getElementsByClassName('example');
     for(var i = 0, l = block.length; i < l; i++) {
-        block[i].innerHTML = block[i].innerHTML.replace(reSearch, reReplace);
+	// highlight `user@hostname $'
+        block[i].innerHTML = block[i].innerHTML.replace(/^(\w*@\w*)\s*([:](.+)\/([^/]+)[$])/, userHighlight);
+	// highlight `root@hostname #'
+        block[i].innerHTML = block[i].innerHTML.replace(/^(root@\w*)\s*([:](.+)\/([^/]+)[#])/, rootHighlight);
+	// highlight `hostname #'
+        block[i].innerHTML = block[i].innerHTML.replace(/^(\w*)\s*([:](.+)\/([^/]+)[#])/, rootHighlight);
     }
 }
-
 
 $(document).ready(function () {
     colorShellPrompt();
