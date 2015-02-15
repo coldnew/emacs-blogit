392c3806df0a6acb53c4c740b4413385767f8997
Yen-Chin Lee <coldnew.tw@gmail.com>
2015-02-15 19:20:56 +0800

    Add paragraph generate method
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

--- emacs-blogit-pelican.el
+++ emacs-blogit-pelican.el
@@ -34,6 +34,89 @@
 (require 'ox-publish)
 
 
+
+;;;; Backend
+
+(org-export-define-derived-backend 'blogit-pelican 'html
+  :translate-alist
+  '(
+    ;; Fix for multibyte language
+    (paragraph . org-blogit-pelican-paragraph)
+    ;; Fix toc for blogit theme
+    (inner-template . org-blogit-pelican-inner-template)
+    ))
+
+
+;;;; Paragraph
+
+(defun org-blogit-pelican-paragraph (paragraph contents info)
+  "Transcode PARAGRAPH element into Markdown format.
+CONTENTS is the paragraph contents.  INFO is a plist used as
+a communication channel."
+  ;; Fix multibyte language like chinese will be automatically add
+  ;; some space since org-mode will transpose auto-fill-mode's space
+  ;; to newline char.
+  (let* ((fix-regexp "[[:multibyte:]]")
+         (fix-contents
+          (replace-regexp-in-string
+           (concat "\\(" fix-regexp "\\) *\n *\\(" fix-regexp "\\)") "\\1\\2" contents))
+         ;; Unfill paragraph to make contents look mode better
+         (unfill-contents
+          (with-temp-buffer
+            (insert fix-contents)
+            (replace-regexp "\\([^\n]\\)\n\\([^ *\n]\\)" "\\1 \\2" nil (point-min) (point-max))
+            (buffer-string))))
+
+    ;; Send modify data to org-md-paragraph
+    (org-html-paragraph paragraph unfill-contents info)))
+
+
+;;; Template
+
+(defun org-blogit-pelican-inner-template (contents info)
+  "Return body of document string after HTML conversion.
+CONTENTS is the transcoded contents string.  INFO is a plist
+holding export options."
+  (concat
+   ;; Table of contents.
+   (let ((depth (plist-get info :with-toc)))
+     (when depth (org-blogit-pelican-toc depth info)))
+   ;; Document contents.
+   contents
+   ;; Footnotes section.
+   (org-html-footnote-section info)))
+
+
+;;; Tables of Contents
+
+(defun org-blogit-pelican-toc (depth info)
+  "Build a table of contents.
+DEPTH is an integer specifying the depth of the table.  INFO is a
+plist used as a communication channel.  Return the table of
+contents as a string, or nil if it is empty."
+  (let ((toc-entries
+         (mapcar (lambda (headline)
+                   (cons (org-html--format-toc-headline headline info)
+                         (org-export-get-relative-level headline info)))
+                 (org-export-collect-headlines info depth))))
+    (when toc-entries
+      (format "<div class=\"table-of-contents\">\n\n"))))
+
+
+;;; End-user functions
+
+;;;###autoload
+(defun blogit-export-as-pelican
+    (&optional async subtreep visible-only body-only ext-plist)
+  "Export current buffer to an HTML buffer for blogit.
+
+Export is done in a buffer named \"*Blogit HTML Export*\", which
+will be displayed when `org-export-show-temporary-export-buffer'
+is non-nil."
+  (interactive)
+  (org-export-to-buffer 'blogit-perlican "*Blogit perlican Export*"
+    async subtreep visible-only body-only ext-plist
+    (lambda () (html-mode))))
 
 
 
