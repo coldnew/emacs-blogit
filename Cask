c9acf6efb942055f9ae644e80cb98b78a0df42fb
Yen-Chin Lee <coldnew.tw@gmail.com>
2015-02-15 18:59:11 +0800

    Add prototype of emacs-blogit-pelican
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

---
+++ Cask
@@ -0,0 +1,13 @@
+(source melpa)
+(source org)
+
+(package-file "emacs-blogit-pelican.el")
+
+(depends-on "org-plus-contrib")
+(depends-on "f")
+(depends-on "s")
+
+(development
+ (depends-on "ert-runner")
+ (depends-on "ecukes")
+ (depends-on "espuds"))
---
+++ emacs-blogit-pelican.el
@@ -0,0 +1,42 @@
+;;; emacs-blogit-pelican.el --- Export org-mode to pelican.
+
+;; Copyright (c) 2015 Yen-Chin, Lee. (coldnew) <coldnew.tw@gmail.com>
+;;
+;; Author: coldnew <coldnew.tw@gmail.com>
+;; Keywords:
+;; X-URL: http://github.com/coldnew/emacs-blogit-pelican
+;; Version: 0.1
+;; Package-Requires: ((org "8.0") (cl-lib "0.5") (f "0.17.2"))
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program; if not, write to the Free Software
+;; Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+;;; Commentary:
+
+;;; Code:
+
+(eval-when-compile (require 'cl-lib))
+
+(require 'ox-html)
+(require 'ox-publish)
+
+
+
+
+
+
+(provide 'emacs-blogit-pelican)
+;;; emacs-blogit-pelican.el ends here.
